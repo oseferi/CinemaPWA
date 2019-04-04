b29d3975a5dc8bcaaa3e982e1cf2e032054e9b57
Elton Memishaj <eltoni.memishi@live.com>
2019-04-04 12:09:43 +0200

    + Created "createMovie", "updateMovie", "deleteMovie" and "restoreMovie" public methods to MovieService. - Updated ScheduleService to match the real API.

--- src/app/home/movies/services/movie.service.ts
+++ src/app/home/movies/services/movie.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { Movie } from '../models/movie.model';
+import { Update } from '@ngrx/entity';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +12,8 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   public getMovies = (): Observable<Movie[]> => this.http.get<Movie[]>(`${environment.apiUrl}/movies`);
+  public createMovie = (request: Partial<Movie>): Observable<Movie> => this.http.post<Movie>(`${environment.apiUrl}/movies`, request);
+  public updateMovie = (request: Update<Movie>): Observable<Movie> => this.http.patch<Movie>(`${environment.apiUrl}/movies/${request.id}`, request.changes);
+  public deleteMovie = (id: string): Observable<void> => this.http.delete<void>(`${environment.apiUrl}/movies/${id}`);
+  public restoreMovie = (id: string): Observable<void> => this.http.patch<void>(`${environment.apiUrl}/movies/${id}/restore`, {});
 }
--- src/app/home/schedules/services/schedule.service.ts
+++ src/app/home/schedules/services/schedule.service.ts
@@ -11,9 +11,9 @@ import { Update } from '@ngrx/entity';
 export class ScheduleService {
   constructor(private http: HttpClient) {}
 
-  public getSchedules = (): Observable<Schedule[]> => this.http.get<Schedule[]>(`${environment.apiUrl}/schedules?validity=true`);
-  public createSchedule = (request: Partial<Schedule>): Observable<Schedule> => this.http.post<Schedule>(`${environment.apiUrl}/schedules`, { ...request, validity: true });
+  public getSchedules = (): Observable<Schedule[]> => this.http.get<Schedule[]>(`${environment.apiUrl}/schedules`);
+  public createSchedule = (request: Partial<Schedule>): Observable<Schedule> => this.http.post<Schedule>(`${environment.apiUrl}/schedules`, request);
   public updateSchedule = (request: Update<Schedule>): Observable<Schedule> => this.http.patch<Schedule>(`${environment.apiUrl}/schedules/${request.id}`, request.changes);
-  public deleteSchedule = (id: string): Observable<void> => this.http.patch<void>(`${environment.apiUrl}/schedules/${id}`, { validity: false });
-  public restoreSchedule = (id: string): Observable<void> => this.http.patch<void>(`${environment.apiUrl}/schedules/${id}`, { validity: true });
+  public deleteSchedule = (id: string): Observable<void> => this.http.delete<void>(`${environment.apiUrl}/schedules/${id}`);
+  public restoreSchedule = (id: string): Observable<void> => this.http.patch<void>(`${environment.apiUrl}/schedules/${id}/restore`, {});
 }
